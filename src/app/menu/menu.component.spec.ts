c67755986c7aa787a6788fbe799310a1a0102f32
Sroka <tomasz.sroka@capgemini.com>
2018-09-17 11:50:30 +0200

    working site without tests

--- src/app/menu/menu.component.spec.ts
+++ src/app/menu/menu.component.spec.ts
@@ -1,17 +1,36 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MenuComponent } from './menu.component';
+import {MenuService} from '../menu.service';
+import {HttpClientModule} from '@angular/common/http';
+import {CartComponent} from '../cart/cart.component';
+import {CartService} from '../cart.service';
+import {OrderService} from '../order.service';
+import {of} from 'rxjs';
 
-describe('MenuComponent', () => {
-  let component: MenuComponent;
-  let fixture: ComponentFixture<MenuComponent>;
+fdescribe('MenuComponent', () => {
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ MenuComponent ]
-    })
-    .compileComponents();
-  }));
+    let component: MenuComponent;
+    let fixture: ComponentFixture<MenuComponent>;
+
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [MenuComponent,
+          CartComponent],
+        imports: [
+          HttpClientModule
+        ],
+        providers: [
+          MenuService,
+          CartService,
+          OrderService
+        ]
+      })
+        .compileComponents();
+      const menuService = TestBed.get(MenuService);
+      const getMenuSpy = spyOn(menuService, 'getMenu').and.returnValue(of([]));
+      const getPizzaSpy = spyOn(menuService, 'getPizza').and.returnValue(of([]));
+    }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(MenuComponent);
@@ -22,4 +41,14 @@ describe('MenuComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  it('should open getPizza on ngOnInit', () => {
+    const getPizzaSpyComponent = spyOn(component, 'getPizza').and.returnValue(of(null));
+
+    component.ngOnInit();
+
+    expect(getPizzaSpyComponent).toHaveBeenCalled();
+  });
+
 });
