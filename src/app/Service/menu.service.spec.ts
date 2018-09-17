a4acfe791878881f579634639eeae10a5b198af0
Sroka <tomasz.sroka@capgemini.com>
2018-09-17 19:30:29 +0200

    Menu Service tests

--- src/app/Service/menu.service.spec.ts
+++ src/app/Service/menu.service.spec.ts
@@ -1,15 +1,59 @@
-import { TestBed, inject } from '@angular/core/testing';
+import {fakeAsync, TestBed} from '@angular/core/testing';
 
-import { MenuService } from './menu.service';
+import {MenuService} from './menu.service';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {MenuEntry} from '../Model/MenuEntry.Model';
+import {MenuType} from '../Model/MenuType.Enum';
+import {Observable, of} from 'rxjs';
 
-describe('MenuService', () => {
+
+fdescribe('MenuService', () => {
+  let service: MenuService;
+  let testingController: HttpTestingController;
+  const URL = 'http://localhost:3000/MenuEntry';
+
+  const pizza: MenuEntry = {
+    id: 31,
+    name: 'Pizza',
+    isAvailable: true,
+    description: 'Zwykła pizza',
+    type: MenuType.PIZZA,
+    price: 32,
+  };
+
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [MenuService]
+      providers: [MenuService,
+      ],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule
+      ]
     });
+
+    service = TestBed.get(MenuService);
+    testingController = TestBed.get(HttpTestingController);
   });
 
-  it('should be created', inject([MenuService], (service: MenuService) => {
+  it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+
+  it('should menu will be equal mocked', fakeAsync(() => {
+    let dishes: MenuEntry[] = [];
+    const mockMenu: MenuEntry[] = [pizza, pizza, pizza, pizza, pizza, pizza, pizza];
+
+    // when
+    service.getMenu().subscribe(x => dishes = x);
+    const req = testingController.expectOne('/api/MenuEntry');
+    expect(req.request.method).toEqual('GET');
+    req.flush(mockMenu);
+
+    expect(dishes).toEqual(mockMenu);
+
+    testingController.verify();
   }));
 });
